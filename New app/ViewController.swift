1589f37890f3d959f00f4409ec7a5ff063a11484
Dimida <dimida.badmaeva@mail.ru>
2025-10-16 00:00:17 +0300

    Initial commit

---
+++ New app/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  New app
+//
+//  Created by Димида Бадмаева on 13/10/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var changeButton: UIButton!
+    @IBOutlet weak var skyImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        skyImageView.image = UIImage(systemName: "moon")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ New appTests/New_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  New_appTests.swift
+//  New appTests
+//
+//  Created by Димида Бадмаева on 13/10/2025.
+//
+
+import Testing
+@testable import New_app
+
+struct New_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
