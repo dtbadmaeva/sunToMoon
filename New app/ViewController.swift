9fce0c7e8c0ffae20eb5e79a1715aa707793293e
Dimida <dimida.badmaeva@mail.ru>
2025-10-16 17:41:46 +0300

    animation

--- New app/ViewController.swift
+++ New app/ViewController.swift
@@ -11,14 +11,35 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var changeButton: UIButton!
     @IBOutlet weak var skyImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Начальное состояние
         skyImageView.image = UIImage(systemName: "moon")
         skyImageView.tintColor = .gray
         changeButton.tintColor = .green
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonDidTap(_ sender: Any) {
+        // Проверяем, какая сейчас иконка
+        let isMoon = skyImageView.image == UIImage(systemName: "moon")
+        
+        // Выбираем новую иконку
+        let newIcon = isMoon ? "sun.max.fill" : "moon"
+        let newColor = isMoon ? UIColor.systemYellow : UIColor.gray
+        
+        // Анимация плавного перехода
+        
+        UIView.transition(with: skyImageView,
+                          duration: 0.5,
+                          options: .transitionCrossDissolve,
+                          animations: {
+                              self.skyImageView.image = UIImage(systemName: newIcon)
+                              self.skyImageView.tintColor = newColor
+                          },
+                          completion: nil)
+    }
 }
+
 
